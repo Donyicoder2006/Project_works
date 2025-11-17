ba1a846d3b0bcf3872369731a6850cb8b54042aa
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2025-11-17 13:50:36 +0530

    feat: add feedback chart

--- frontend/app/components/charts/feedback-chart.tsx
+++ frontend/app/components/charts/feedback-chart.tsx
@@ -1,9 +1,23 @@
 "use client";
 import { feedbackApi, healthApi } from "@/app/types/api-types";
 import { mainFormData } from "@/app/types/main-form-data";
+import { capitalize } from "@/app/utils/text";
 import { Card, CardBody } from "@heroui/react";
+import { StarIcon } from "lucide-react";
 import { useFormContext } from "react-hook-form";
 import useSWR from "swr";
+
+enum Colors {
+  Green = "#a3e635",
+  Yellow = "#facc15",
+  Orange = "#fb923c",
+}
+
+enum FeedBackStates {
+  Poor = "poor feedback",
+  Medium = "median feedback",
+  Excellent = "excellent feedback",
+}
 
 const fetcher = ([url, payload]: [url: string, payload: mainFormData]) =>
   fetch(url, {
@@ -25,12 +39,59 @@ const FeedbackChart = () => {
     fetcher
   );
 
+  if (!data) return <div>Not found</div>;
+
+  const feedbackState = data.feedback_prediction;
+
+  const selectedColor =
+    feedbackState === FeedBackStates.Excellent
+      ? Colors.Green
+      : feedbackState === FeedBackStates.Medium
+      ? Colors.Yellow
+      : feedbackState === FeedBackStates.Poor
+      ? Colors.Orange
+      : "black";
+
   return (
     <Card>
-      <CardBody>
-        <p>Data: {JSON.stringify(data)}</p>
-        <p>Error: {JSON.stringify(error)}</p>
-        <p>Is Loading: {JSON.stringify(isLoading)}</p>
+      <CardBody className="flex items-center justify-center">
+        <h3 className="self-start">Rating Prediction</h3>
+        <div className="flex space-x-2">
+          <StarIcon color={selectedColor} fill={selectedColor} size={60} />
+          <StarIcon
+            color={
+              [FeedBackStates.Excellent, FeedBackStates.Medium].includes(
+                feedbackState as FeedBackStates
+              )
+                ? selectedColor
+                : "darkgrey"
+            }
+            fill={
+              [FeedBackStates.Excellent, FeedBackStates.Medium].includes(
+                feedbackState as FeedBackStates
+              )
+                ? selectedColor
+                : "darkgrey"
+            }
+            size={60}
+          />
+          <StarIcon
+            color={
+              feedbackState === FeedBackStates.Excellent
+                ? selectedColor
+                : "darkgrey"
+            }
+            fill={
+              feedbackState === FeedBackStates.Excellent
+                ? selectedColor
+                : "darkgrey"
+            }
+            size={60}
+          />
+        </div>
+        <p className={`text-2xl`} style={{ color: selectedColor }}>
+          {capitalize(feedbackState)}
+        </p>
       </CardBody>
     </Card>
   );
